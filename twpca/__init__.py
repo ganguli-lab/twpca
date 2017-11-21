c5f353a0f7c1461a28948bdd433eca33f1e08b36
Alex Williams <alex.h.willia@gmail.com>
2017-11-21 12:57:44 -0800

    prototype

--- twpca/__init__.py
+++ twpca/__init__.py
@@ -9,6 +9,5 @@ __version__ = '0.0.3'
 
 from .model import TWPCA
 from . import datasets
-from . import warp
 
 __all__ = ['TWPCA']
--- twpca/model.py
+++ twpca/model.py
@@ -1,18 +1,19 @@
 import numpy as np
 from sklearn.decomposition import TruncatedSVD, NMF
-from .warp import dba_align#, apply_warp
+from dtw import fastdtw
+from scipy.interpolate import interp1d
+from tqdm import tqdm
 
 class TWPCA(object):
-    def __init__(self, n_components, nonneg=False):
+    def __init__(self, n_components, share_warps=True, nonneg=False):
 
         # model options
         self.n_components = n_components
+        self.share_warps = share_warps
         self.nonneg = nonneg
 
         # attributes set by calling fit
         self.warping_funcs = None
-
-        # TODO warps for each component
 
     def fit(self, data):
         """Fits warps and components.
@@ -32,13 +33,16 @@ class TWPCA(object):
         decomp = D(n_components=self.n_components)
         
         # tensor of low-dimensional time series (K x T x n_components)
-        self.V = decomp.fit_transform(matrix).reshape(K, T, n_components)
+        self.V = decomp.fit_transform(matrix).reshape(K, T, self.n_components)
         
         # projection into low-dimensional space (N x n_components matrix)
         self.U = np.transpose(decomp.components_)
 
         # do time warping in low-dimensional space
-        self.V_template, self.warping_funcs, self.optim_hist = dba_align(V)
+        if self.share_warps:
+            self.warping_funcs = _dtw_median(self.V) # warp all components together
+        else:
+            self.warping_funcs = np.array([_dtw_median(v) for v in self.V.T]) # warp each component separately
 
 
     def transform(self, data):
@@ -47,8 +51,48 @@ class TWPCA(object):
         if self.warping_funcs is None:
             raise ValueError('Warps are not fit. Must call model.fit(...) before model.transform(...).')
         
-        return warp(data, self.warping_funcs)
+        return _warp(data, self.warping_funcs)
+
+    def fit_transform(self, data):
+        """Fits warps and applies them.
+        """
+        self.fit(data)
+        return self.transform(data)
+
+def _sqerr(x, y):
+    """Squared error cost function.
+    """
+    return np.sum((x - y)**2)
+
+def _warp(data, warping_funcs):
+    """Applies warps
+    """
+    t = np.arange(data.shape[1])
+    aligned_data = []
+    for trial, warp in zip(data, warping_funcs):
+        # f = interp1d(t, trial, axis=0)
+        g = interp1d(np.arange(len(warp)), trial[warp], axis=0)
+        aligned_data.append(g(np.linspace(0, len(warp)-1, len(t))))
+
+    return np.array(aligned_data)
+
+def _dtw_median(data):
+    """Warps all trials to a template.
+    """
+
+    # choose template trial (closest to trial-average)
+    err_per_trial = np.sum((data - np.mean(data, axis=0))**2, axis=(1,2))
+    template = data[np.argmin(err_per_trial)]
+
+    # fit warps
+    iterator = tqdm
+    warps = np.array([fastdtw(trial, template, dist=_sqerr)[-1][0] for trial in iterator(data)])
+
+    # for iter in range(3):
+    #     template = np.mean(_warp(data, warps), axis=0)
+    #     warps = np.array([fastdtw(trial, template, dist=_sqerr)[-1][0] for trial in iterator(data)])
+
+    return warps
 
 
 
-# derivative of tau(V) with respect to 
